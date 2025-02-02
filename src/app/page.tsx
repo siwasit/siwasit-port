b2f8e49840dd808644ced26fca72186172157693
siwasit <siwasit2546@gmail.com>
2025-02-02 19:23:16 +0700

    add icon and meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,26 +35,6 @@ export default function Home() {
                 </button>
               </li>
             </ul>
-
-            {/* Hamburger Menu for Mobile */}
-            {/* <div className="md:hidden">
-            <button className="focus:outline-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            </button>
-          </div> */}
           </div>
         </nav>
       </header>
